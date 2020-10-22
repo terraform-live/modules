1dcb791c9ec28561ed0ef02b8cebac8a33d771c3
Eze Onuoha <onuohaeze@gmail.com>
2020-10-22 09:44:36 -0400

    fix user-data

--- services/webserver-cluster/user-data.sh
+++ services/webserver-cluster/user-data.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-cat > /var/www/html/index.html <<EOF
+/usr/bin/sudo yum -y install httpd
+/usr/bin/sudo chkconfig --add  httpd
+/usr/bin/sudo service httpd start
+/usr/bin/sudo sed -i 's/Listen 80/Listen 8080/' /etc/httpd/conf/httpd.conf
+
+/bin/cat > index.html <<EOF
 <h1>This is a Great Day</h1>
 <p>DB address: ${db_address}</p>
 <p>DB port: ${db_port}</p>
 EOF
 
-sudo yum -y install httpd
-sudo chkconfig --add  httpd
-sudo service httpd start
-sudo sed -i 's/Listen 80/Listen 8080/' /etc/httpd/conf/httpd.conf
-sudo service http restart
+/usr/bin/sudo mv index.html /var/www/html/
+
+/usr/bin/sudo service restart httpd
