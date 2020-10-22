9684bfd5d0e92249804827a397bc9e2214fe2b98
Eze Onuoha <onuohaeze@gmail.com>
2020-10-21 23:09:08 -0400

    user-data changes

--- services/webserver-cluster/user-data.sh
+++ services/webserver-cluster/user-data.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-cat > index.html <<-EOF
-<h1>Hello, World. The Lord is Good"</h1>
-<p>DB address: ${db_address}</p>
-<p>DB port: ${db_port}</p> 
-EOF
-
-nohup busybox httpd -f -p ${server_port} &
+sudo apt update
+sudo apt upgrade
+sudo apt install nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
+sudo ufw allow http
+sudo ufw enable
+sed -i 's/80/8080/g' /etc/nginx/sites-enabled/default
+sudo systemctl start nginx
