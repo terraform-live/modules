bcfbf637fda24ac8ad7f8aa89c34032db6b56dd2
Eze Onuoha <onuohaeze@gmail.com>
2020-10-24 19:47:59 -0400

    Fix userdata

--- services/webserver-cluster/user-data.sh
+++ services/webserver-cluster/user-data.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 cat > index.html <<EOF
-<h1>$This is the Day The Lord has Made</h1>
+<h1>Hello, World</h1>
 <p>DB address: ${db_address}</p>
 <p>DB port: ${db_port}</p>
 EOF
 
-nohup busybox httpd -f -p ${server_port} & > busybox.log 2>&1
+nohup busybox httpd -f -p ${server_port} &
