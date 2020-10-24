e940339435675d4d7bcecca78b38931246df3080
Eze Onuoha <onuohaeze@gmail.com>
2020-10-24 19:07:10 -0400

    update userdata

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -202,9 +202,9 @@ resource "aws_lb_target_group" "asg" {
 		protocol = "HTTP"
 		matcher  = "200"
 		interval = 15
-		timeout	 = 5
-		healthy_threshold   = 10
-		unhealthy_threshold = 10
+		timeout	 = 3
+		healthy_threshold   = 2
+		unhealthy_threshold = 2
 	}
 }
 
--- services/webserver-cluster/user-data.sh
+++ services/webserver-cluster/user-data.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 cat > index.html <<EOF
 <h1>$This is the Day The Lord has Made</h1>
 <p>DB address: ${db_address}</p>
 <p>DB port: ${db_port}</p>
 EOF
 
-nohup busybox httpd -f -p 8080 &
+nohup busybox httpd -f -p ${server_port} & > busybox.log 2>&1
