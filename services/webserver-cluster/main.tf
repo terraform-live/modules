2adb14af147b1378378d586d9adbc13b877e70b9
Eze Onuoha <onuohaeze@gmail.com>
2020-10-25 19:19:37 -0400

    fix

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -314,13 +314,13 @@ resource "aws_iam_policy" "cloudwatch_full_access" {
 resource "aws_iam_user_policy_attachment" "neo_cloudwatch_full_access" {
   count = var.give_neo_cloudwatch_full_access ? 1 : 0
 
-  user = aws_iam_user.example[0].name
+  user = aws_iam_user.example[*].name
   policy_arn = aws_iam_policy.cloudwatch_full_access.arn
 }
 resource "aws_iam_user_policy_attachment" "neo_cloudwatch_read_only" {
   count = var.give_neo_cloudwatch_full_access ? 0 : 1
 
-  user = aws_iam_user.example[0].name
+  user = aws_iam_user.example[*].name
   policy_arn = aws_iam_policy.cloudwatch_read_only.arn
 }
 resource "aws_iam_user" "example" {
