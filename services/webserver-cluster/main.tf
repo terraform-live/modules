0451acb8d966bcc51f68ebe134a5bf0479575db0
Eze Onuoha <onuohaeze@gmail.com>
2020-11-16 20:20:53 -0500

    Fix main.tf

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -137,14 +137,27 @@ resource "aws_launch_configuration" "example" {
 }
 
 resource "aws_autoscaling_group" "example" {
+  #Explicitly depend on the launch configuration's name so each time it's
+  # replaced, this ASG is also replaced
+  name = "{var.cluster}-${aws_launch_configuration.example.name}"
   launch_configuration = aws_launch_configuration.example.name  
   vpc_zone_identifier  = data.aws_subnet_ids.default.ids
       
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
 
+  # Wait for at least this many instances to pass health checks before considering 
+  # the ASG deployment complete
+  min_elb_capacity = var.min_size
+
   min_size = var.min_size
   max_size = var.max_size
+
+  # When replacing this ASG, create the replacement first, and only delete the 
+  # original after
+  lifecycle {
+    create_before_destroy = true
+  }
 
   tag {
      key = "Name"
